5716a9739880a19c7e69a69c1e5ff4da028f5091
joohee6218 <jhkim@coinplug.com>
2024-09-02 19:45:23 +0900

    edited

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 import openai
 from dotenv import load_dotenv
+from utils.logs_utils import LoggerUtility
+from config import *
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
